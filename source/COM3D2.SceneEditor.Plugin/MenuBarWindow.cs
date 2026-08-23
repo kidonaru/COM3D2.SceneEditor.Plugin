aa792e6f8cb42c1ce7894a8e0822a164b65426d0
kidonaru <kidonaru@gmail.com>
2026-08-23 09:55:03 +0900

    Revert "feat(timeline): レイヤー設定ウィンドウを追加 (currentLayer.DrawWindow を接続)"
    This reverts commit 162de262d1c2a2c2e0663fede4b8f5e4e8d281f8.

--- source/COM3D2.SceneEditor.Plugin/MenuBarWindow.cs
+++ source/COM3D2.SceneEditor.Plugin/MenuBarWindow.cs
@@ -130,7 +130,6 @@ namespace COM3D2.SceneEditor.Plugin
                         CreateWindowItem("PNG配置", PngPlacementWindow.instance),
                         CreateWindowItem("プリセット", PresetWindow.instance),
                         CreateWindowItem("タイムライン", TimelineWindow.instance),
-                        CreateWindowItem("レイヤー設定", TimelineLayerWindow.instance),
                         CreateWindowItem("操作履歴", HistoryWindow.instance),
                         CreateWindowItem("設定", SettingWindow.instance),
                     },
